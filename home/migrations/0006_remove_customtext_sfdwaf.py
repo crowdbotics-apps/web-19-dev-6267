bc74efefc0ea4f1eb59ff8e98fc4778827bbc74f
Crowdbotics <team@crowdbotics.com>
2020-06-19 05:19:45 +0000

    Update models

--- home/migrations/0006_remove_customtext_sfdwaf.py
+++ home/migrations/0006_remove_customtext_sfdwaf.py
@@ -6,12 +6,9 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0005_auto_20200619_0516'),
+        ("home", "0005_auto_20200619_0516"),
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='customtext',
-            name='sfdwaf',
-        ),
+        migrations.RemoveField(model_name="customtext", name="sfdwaf",),
     ]
--- home/models.py
+++ home/models.py
@@ -8,6 +8,8 @@ from django.db import models
 
 class CustomText(models.Model):
     title = models.CharField(max_length=150,)
+    jhglkhgljg = models.BigIntegerField(null=True, blank=True,)
+    fgwsrg = models.TextField(null=True, blank=True,)
 
     def __str__(self):
         return self.title
