70edb6c8a1647b185e8a6d3fae5c14f7474f7cdf
Crowdbotics <team@crowdbotics.com>
2020-06-19 05:15:18 +0000

    Update models

--- home/migrations/0003_auto_20200619_0514.py
+++ home/migrations/0003_auto_20200619_0514.py
@@ -6,18 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('home', '0002_load_initial_data'),
+        ("home", "0002_load_initial_data"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='customtext',
-            name='sdfwafe',
+            model_name="customtext",
+            name="sdfwafe",
             field=models.BigIntegerField(blank=True, null=True),
         ),
         migrations.AddField(
-            model_name='customtext',
-            name='sfdwaf',
+            model_name="customtext",
+            name="sfdwaf",
             field=models.TextField(blank=True, null=True),
         ),
     ]
